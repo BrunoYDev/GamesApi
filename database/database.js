f57a959bcd38a03427a828f3603dcf48a758da92
Bruno Rodrigues Garcia <118909147+BrunoYDev@users.noreply.github.com>
2024-09-04 11:18:36 -0300

    Change: sqlite directory Remove: Sqlite file from commits

--- database/database.js
+++ database/database.js
@@ -1,6 +1,6 @@
 const sqlite3 = require("sqlite3").verbose();
 
-let db = new sqlite3.Database("./Games.sqlite", (err) => {
+let db = new sqlite3.Database("./database/games.sqlite", (err) => {
   if (err) {
     console.error(err.message);
   }
